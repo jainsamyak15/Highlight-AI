a7f43961a1e2c5f70a64657f2e488400ca3453b6
Samyak <samyak15.sj@gmail.com>
2025-02-28 22:08:02 +0530

    almost done

--- backend/models/schemas.py
+++ backend/models/schemas.py
@@ -2,9 +2,15 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 
+
 class Highlight(BaseModel):
     text: str
     summary: Optional[str] = None
     url: Optional[str] = None
     user_id: str
-    created_at: datetime = datetime.utcnow()
+    created_at: str = datetime.utcnow().isoformat()
+
+    class Config:
+        json_encoders = {
+            datetime: lambda v: v.isoformat()
+        }
